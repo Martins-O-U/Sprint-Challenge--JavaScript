a0e4fbb6379b86032e72c92cad1c7704854eff86
Martins-O-U <martinsonyedikachi@gmail.com>
2019-08-23 12:16:30 +0100

    strech completed

--- challenges/classes.js
+++ challenges/classes.js
@@ -23,49 +23,25 @@ console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  
+// Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  
+// Test your work by logging out your volume and surface area.
 
-// class Person {
-//     constructor(Name, Age) {
-//       this.name = Name;
-//       this.age = Age;
-//       this.eatEdibles = true;
-//       this.stomach = [];
-//       this.poop = true;
-//     }
-//     greet() {
-//         return `Hello, my name is ${this.name} and i am ${this.age} years old.`
-//     }
-//     pooping() {
-//         if(this.poop === true) {
-//             return this.stomach =[]
-//           }    
-//         }
-//     eats(meals) {
-//         if(this.eatEdibles === true){
-//             return this.stomach.push(meals);
-//           }else{
-//             return `Can't eat that, please.`
-//           };
-//         };
-//     }
-
-//   const Humn = new Person("James", 40)
-//   console.log("Name: "+ Humn.name);
-//   console.log("Age: " + Humn.age);
-//   console.log(Humn.greet());
-//   console.log(Humn.eats());
-//   console.log(Humn.pooping());
-
-
-//   class Baby extends Person {
-//     constructor(Name, Age, playWith) {
-//       super(Name, Age);
-//       this.favoriteToy = playWith;
-//     }
-//     playWithToy() {
-//         return `I love playing with my ${this.favoriteToy}!`;
-//       };
-//   }
-// const baby = new Baby("Cindy", 5, "Barbie")
-// console.log(baby.playWithToy());
+class CubeMaker extends CuboidMakers {
+    //surface Area = 6 * a * a
+    //volume a *a *a
+    constructor(length, width,height,edge,){
+        super(length, width,height);
+        this.edge = edge;
+    }
+    cubeSurfaceArea(edge){
+        let Area = 6 *(this.edge * this.edge);
+        return Area;
+    }
+    cubeVolume(edge){
+        let volume = (this.edge * this.edge * this.edge)
+        return volume;
+    }
+}
+const cubes = new CubeMaker(1,2,3,4)
+ console.log(cubes.cubeVolume());
